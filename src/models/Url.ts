520edf80b593a34027d8b60059e9b03cd3120724
Engr Francis <40895902+audenna@users.noreply.github.com>
2025-02-12 13:40:06 +0100

    Updated

--- src/models/Url.ts
+++ src/models/Url.ts
@@ -3,18 +3,12 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IUrl extends Document {
     originalUrl: string;
     shortUrl: string;
-    alias?: string;
-    clicks: number;
-    expiresAt?: Date;
     createdAt: Date;
 }
 
 const UrlSchema = new Schema<IUrl>({
     originalUrl: { type: String, required: true },
     shortUrl: { type: String, required: true, unique: true },
-    alias: { type: String, unique: true, sparse: true },
-    clicks: { type: Number, default: 0 },
-    expiresAt: { type: Date, default: null },
     createdAt: { type: Date, default: new Date() },
 });
 
