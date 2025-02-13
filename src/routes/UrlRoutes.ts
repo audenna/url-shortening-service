8b91bfd03e81cbf26929a3d4da456c4e48b7799f
Engr Francis <40895902+audenna@users.noreply.github.com>
2025-02-13 21:49:01 +0100

    Fixed issues

--- src/routes/UrlRoutes.ts
+++ src/routes/UrlRoutes.ts
@@ -4,7 +4,7 @@ import { validateUrlPayload } from "../middlewares/validateUrlPayload";
 import { ValidateSocketHeader } from "../middlewares/validateSocketHeader";
 
 const router = Router();
-const urlController = UrlController.getInstance(); // Get the singleton instance
+const urlController: UrlController = UrlController.getInstance(); // Get the singleton instance
 
 // Define the required routes below
 router.post("/url", ValidateSocketHeader, validateUrlPayload, urlController.postUrl);
